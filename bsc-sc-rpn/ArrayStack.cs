cd7fc06f29241ea18acb555ab7cb49694322f883
dmac-afk <107273919+dmac-afk@users.noreply.github.com>
2026-02-18 19:22:27 +0000

    ArrayStack<T> with IStack<T> interface implemented

---
+++ bsc-sc-rpn/ArrayStack.cs
@@ -0,0 +1,34 @@
+﻿namespace bsc_sc_rpn
+{
+    internal class ArrayStack<T> : IStack<T>
+    {
+        private readonly T[] _items;
+        private int _top;
+        private readonly int _capacity;
+
+        public ArrayStack(int capacity)
+        {
+            _capacity = capacity;
+            _items = new T[_capacity];
+            _top = -1; // -1 means the stack is empty
+        }
+
+        // Pushes an item onto the top of the stack.
+        public void Push(T item)
+        {
+            _items[++_top] = item; // Increment top and add item to stack, increment first to avoid writing to index -1 which is invalid. 
+        }
+
+        // Removes and returns the item at the top of the stack.
+        public T Pop()
+        {
+            T item = _items[_top];
+            _items[_top--] = default; // Use default to clear value.
+            return item;
+        }
+
+        public T Peek() => _items[_top];
+
+        public bool IsEmpty() => _top == -1;
+    }
+}
